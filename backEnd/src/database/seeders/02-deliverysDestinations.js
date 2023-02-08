98cf713e8d8d93f3ff7fc3ec6a2e0f45d8ff69a5
Breno Nascimento <satsukebare23@gmail.com>
2023-02-08 13:28:43 -0300

    add seeders in script and delivery destinations seeders

--- backEnd/src/database/seeders/02-deliverysDestinations.js
+++ backEnd/src/database/seeders/02-deliverysDestinations.js
@@ -3,6 +3,12 @@ module.exports = {
     await queryInterface.bulkInsert(
       'deliverysDestinations',
       [
+        {
+          id: 1,
+          departure: '-15.038343158420444, -41.93284629590619',
+          destiny: '43.7357430958858, -79.37240494303681',
+          deliveryId: 1
+        }
       ],
       {},
     );
