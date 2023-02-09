9804b286cfdfa6ed8b87aa2b2a5d2b9041836bb4
Breno Nascimento <satsukebare23@gmail.com>
2023-02-09 14:28:33 -0300

    craete custom hook

--- frontEnd/src/customHooks/UseFetch.tsx
+++ frontEnd/src/customHooks/UseFetch.tsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+import { Axios, AxiosError, AxiosInstance, Method } from 'axios';
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+import connection from '../services/api.connection';
 
 interface fetchPropsInterface {
-  url: string
+  url: string;
+  method: Method;
 }
 
-export default function UseFetch({ url }: fetchPropsInterface) {
+export default function UseFetch({ url, method = 'GET' }: fetchPropsInterface) {
   const [data, setData] = useState(undefined);
   const [error, setError] = useState(undefined);
   const [loading, setLoading] = useState(false);
+
+  const response = async () => {
+    try {
+      const response = await connection.get(url);
+      setData(response.data);
+      setLoading(false);
+    } catch(e: any) { 
+      toast.error('Falha na conexão');
+      setError(e.response.data.message)
+    };
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    response();
+  }, [url]);
   
   return { data, error, loading }
 };
--- frontEnd/src/redux/modules/delivery/deliveryReducer.ts
+++ frontEnd/src/redux/modules/delivery/deliveryReducer.ts
@@ -1,16 +1,20 @@
+import { DeliveryTypes } from "../../acionTypes";
+
 export const deliveryInitialState = {
   deliverys: [],
 }
 
 export const deliveryInitialActionValue = {
   type: '',
-  payload: '',
+  payload: [],
 }
 
 export default function deliveryReducer(
   state = deliveryInitialActionValue, action = deliveryInitialActionValue
   ) {
     switch(action.type) {
+      case DeliveryTypes.GET_DELIVERYS:
+        return { ...state, deliverys: [...action.payload] }
       default:
         return state;
     }
