630ea4c601e1c63f04c315cba991c5c7b52a53f2
Breno Nascimento <satsukebare23@gmail.com>
2023-02-09 13:45:31 -0300

    crfeate user fetch custm hook

---
+++ frontEnd/src/customHooks/UseFetch.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+interface fetchPropsInterface {
+  url: string
+}
+
+export default function UseFetch({ url }: fetchPropsInterface) {
+  const [data, setData] = useState(undefined);
+  const [error, setError] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+
+  return { data, error, loading }
+}
