2ab26def32235879f2eab7ae1374b6a1c752ee7d
Breno Nascimento <satsukebare23@gmail.com>
2023-02-09 15:52:56 -0300

    add google maps api library

--- frontEnd/src/pages/NewDelivery/index.tsx
+++ frontEnd/src/pages/NewDelivery/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import styles from './styles.module.css';
 
 export default function NewDelivery() {
   return (
-    <main>
+    <main className={styles.new_delivery_container} >
       <form>
+        <h2>Cadastrar entrega</h2>
         <input
-          placeholder='Client'
+          placeholder='Cliente'
+        />
+        <input
+          placeholder="Saida"
+        />
+        <input
+          placeholder="Destino"
         />
       </form>
     </main>
