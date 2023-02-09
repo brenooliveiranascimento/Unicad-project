5a1e06d50abc1af954d69a026939ee347ae7052e
Breno Nascimento <satsukebare23@gmail.com>
2023-02-09 19:39:44 -0300

    add new delivery form

--- frontEnd/src/pages/NewDelivery/NewDelivery.tsx
+++ frontEnd/src/pages/NewDelivery/NewDelivery.tsx
@@ -1,17 +1,20 @@
-import { GoogleMap, LoadScript, StandaloneSearchBox } from '@react-google-maps/api';
+import { LoadScript, StandaloneSearchBox } from '@react-google-maps/api';
 import { useState } from 'react';
 import styles from './styles.module.css';
+import 'react-calendar/dist/Calendar.css';
 
 export default function NewDelivery() {
 
   const [searchBox, setSeartchBox] = useState<google.maps.places.SearchBox>();
-
+  const [client, setClient] = useState('');
   const [exit, setExit] = useState<{lat: number, lng: number}>();
   const [destiny, setDestiny] = useState<{lat: number, lng: number}>();
+  const [deliveryDate, setDeliveryDate] = useState('');
+  const [error, setError] = useState('');
 
   const onLoad = (ref: google.maps.places.SearchBox) => {
     setSeartchBox(ref)
-  }
+  };
 
   const onPlacesChanged = (destination: string) => {
     const places = searchBox!.getPlaces();
@@ -28,29 +31,48 @@ export default function NewDelivery() {
     };
   }
 
+  const handleDate = (dateSelected: string) => {
+    if(new Date(dateSelected) < new Date()) {
+      setError('A data de entrega não pode ser menor que a data atual');
+      return;
+    };
+    setError('')
+    setDeliveryDate(dateSelected)
+  }
+
   return (
     <main className={styles.new_delivery_container} >
       <form>
         <h2>Cadastrar entrega</h2>
-        <input
+        <input  
+          onChange={({ target: { value } }) => setClient(value) }
           placeholder='Cliente'
         />
-                <LoadScript
-          googleMapsApiKey='AIzaSyDrAGiZgxfTandddrIDtqnVK6UXqgoWp1k'
-          libraries={['places']}
-        >
-        <section className={styles.search_container}> 
+          <LoadScript
+            googleMapsApiKey='AIzaSyDrAGiZgxfTandddrIDtqnVK6UXqgoWp1k'
+            libraries={['places']}
+          >
+          <section className={styles.search_container}> 
             <StandaloneSearchBox onLoad={onLoad}
-              onPlacesChanged={() => onPlacesChanged('exit')}
-            >
-              <input className={styles.search_input} placeholder='Saida'/>
-            </StandaloneSearchBox>
-            <StandaloneSearchBox onLoad={onLoad} 
-              onPlacesChanged={() => onPlacesChanged('destiny')}>
-              <input className={styles.search_input} placeholder='Destino'/>
-            </StandaloneSearchBox>
-          </section>
-        </LoadScript>
+              onPlacesChanged={() => onPlacesChanged('exit')}>
+                <input className={styles.search_input} placeholder='Saida'/>
+              </StandaloneSearchBox>
+
+              <StandaloneSearchBox onLoad={onLoad} 
+                onPlacesChanged={() => onPlacesChanged('destiny')}>
+                <input className={styles.search_input} placeholder='Destino'/>
+              </StandaloneSearchBox>
+            </section>
+          </LoadScript>
+          <label>
+            <span>Data de entrega</span>
+            <input
+              value={deliveryDate}
+              onChange={({target}) => handleDate(target.value)}
+              type={'date'}
+            />
+          { error && <span>{error}</span> }
+          </label>
         <button>
           Cadastrar entrega
         </button>
