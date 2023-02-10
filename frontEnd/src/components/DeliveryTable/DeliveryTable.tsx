fbdc797d64cb0d50ba1b2165071a4ed15127d493
Breno Nascimento <satsukebare23@gmail.com>
2023-02-10 12:28:04 -0300

    add redirect to delicvery details

--- frontEnd/src/components/DeliveryTable/DeliveryTable.tsx
+++ frontEnd/src/components/DeliveryTable/DeliveryTable.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 import { DeliveryI } from '../../interfaces/globalState/DeliveryI';
 import GlobalStateI from '../../interfaces/globalState/GlobalStateI';
 
 export default function DeliveryTable() {
-  const {
-    deliverys
-  } = useSelector(({ 
-    deliverys
-  }: GlobalStateI) => deliverys);
+  const dispatch = useDispatch();
+
+  const { deliverys } = useSelector(({ deliverys }: GlobalStateI) => deliverys);
+
+  const handleDelivery = (currDelivery: DeliveryI) => {
+    window.location.href = `/deliveryDetails/${currDelivery.id}`;
+  };
 
   return (
     <table>
@@ -25,7 +28,7 @@ export default function DeliveryTable() {
             let month = new Date(currDelivery.deliveryDate).getMonth();
             let year = new Date(currDelivery.deliveryDate).getFullYear();
             return (
-                <tr onClick={() => alert(currDelivery.client)} key={currDelivery.id}>
+                <tr onClick={() => handleDelivery(currDelivery)} key={currDelivery.id}>
                   <td>{currDelivery.client}</td>
                   <td>{`${day}/${month}/${year}`}</td>
                   <td>{currDelivery.deliverysDestination.departureName}</td>
